22bba7fa96012aab9bcc2866a903b4cfc40e48d6
Briana Gonzalez <brianagonzalez@brianas-air.lan>
2021-11-06 23:28:50 -0500

    css and media query on server page

--- client/src/screens/Server/Server.jsx
+++ client/src/screens/Server/Server.jsx
@@ -40,30 +40,32 @@ function Server(props) {
 
   return (
     <div className='server-container'>
-      <div>
+      <div className='server-title-container'>
         <p>{server.name}</p>
         {props.currentUser?.id === server.user_id && (
           <Link to={`/server/${server.id}/edit`}>
-            <EditIcon />
+            <EditIcon style={{ fontSize: 20 }}/>
           </Link>
         )}
       </div>
-      {props.currentUser ? <Link to={`/server/${server.id}/create-posts`}><button>+ Create Post</button></Link> : <Link to='/login'><button>+ Create Post</button></Link>}
-      <div>
+      <div className ='create-post-btn'>
+        {props.currentUser ? <Link to={`/server/${server.id}/create-posts`}><button><b>+</b> Create Post</button></Link> : <Link to='/login'><button>+ Create Post</button></Link>}
+      </div>
+      <div className='post-container'>
         {posts.map((post) => (
-          <div key={post.id}>
-            <p>{post.user.username}</p>
-            <p>{post.title}</p>
+          <div key={post.id} className='post'>
+            <p className='post-username'>{post.user.username}</p>
+            <p className='post-title'>{post.title}</p>
             {post.image ? <img src={post.image} alt={post.title} /> : ''}
             {post.text ? <p>{post.text}</p> : ''}
             <Link to={`/server/${server.id}/posts/${post.id}`}>
-              <p><ChatBubbleOutlineIcon/> {post.comments.length} Comments</p>
+              <p className='post-comments'><ChatBubbleOutlineIcon/> &nbsp; {post.comments.length} Comments</p>
             </Link>
             <div>
               {props.currentUser?.id === post.user_id && (
                 <>
-                  <Link to={`/server/${server.id}/post/${post.id}/edit`}><EditIcon /></Link>
-                  <button onClick={() => handleDeletePost(post.id)}><DeleteOutlineIcon /></button>
+                  <Link to={`/server/${server.id}/post/${post.id}/edit`}><EditIcon/></Link>
+                  <button onClick={() => handleDeletePost(post.id)}><DeleteOutlineIcon/></button>
                 </>
               )}
             </div>
